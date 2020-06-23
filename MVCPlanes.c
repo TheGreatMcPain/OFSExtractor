4a84d15badf34aa2271c6dbca41102e578ea10cb
TheGreatMcPain <sixsupersonic@gmail.com>
2020-06-23 16:43:49 -0500

    Rewrite MVCPlanes.c with a working search function.

--- MVCPlanes.c
+++ MVCPlanes.c
@@ -1,99 +1,106 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 
-void getOFMDsFromFile(char *fileName, char **OFMDs);
+// Function definitions.
+FILE *searchStringInFile(char *small, int smallSize, int blockSize, FILE *file,
+                         size_t fileSize);
 void *my_memmem(const void *big, size_t big_len, const void *little,
                 size_t little_len);
+static inline size_t min(size_t x, size_t y);
+
+// Globals
+int BLOCK = 1048576;
 
 int main(int argc, char *argv[]) {
-  char **OFMDs;
-  getOFMDsFromFile(argv[1], OFMDs);
-
-  return 0;
-}
-
-void getOFMDsFromFile(char fileName[], char **OFMDs) {
-  size_t blockSize = (1024 * 1024) * 1;
-  size_t posOFMD;
-  int keepgoing;
-  size_t mvcFileSize;
-  char *OFMDptr;
-  char *ofmd;
+  FILE *filePtr;
   char *buffer;
-  FILE *mvcFile;
-  int frameCount;
-  size_t counter = 0;
 
-  mvcFile = fopen(fileName, "rb");
-  fseek(mvcFile, 0L, SEEK_END);
-  mvcFileSize = ftell(mvcFile);
-  fseek(mvcFile, 0L, SEEK_SET);
+  filePtr = fopen(argv[1], "r");
+  fseek(filePtr, 0L, SEEK_END);
+  size_t fileSize = ftell(filePtr);
+  fseek(filePtr, 0L, SEEK_SET);
 
   while (1) {
-    keepgoing = 1;
-    buffer = (char *)malloc(blockSize * sizeof(char));
-    size_t freadValue = fread(buffer, blockSize, 1, mvcFile);
-    size_t nbytes = blockSize;
-
-    if (freadValue == 0) {
-      keepgoing = 0;
-    }
-
-    if ((OFMDptr = my_memmem(buffer, blockSize, "OFMD", 4)) != NULL) {
-      posOFMD = OFMDptr - buffer;
+    if (searchStringInFile("OFMD", 4, BLOCK, filePtr, fileSize) != NULL) {
+      // Replicate output of grep.
+      printf("%ld:OFMD\n", ftell(filePtr));
     } else {
-      posOFMD = -1;
-    }
-
-    // Until we find new posOFMD. Search more of the file.
-    while (posOFMD == -1) {
-      buffer = (char *)realloc(buffer, (blockSize + nbytes) * sizeof(char));
-      size_t xbytes = fread(buffer + nbytes, blockSize, 1, mvcFile);
-      nbytes += blockSize;
-      if ((OFMDptr = my_memmem(buffer, nbytes, "OFMD", 4)) != NULL) {
-        posOFMD = OFMDptr - buffer;
-      } else {
-        posOFMD = -1;
-      }
-
-      // TODO: When we reach the end of the file stop the loops.
-      if (xbytes <= 0) {
-        keepgoing = 0;
-      }
-    }
-
-    // Seek file to the OFMD's location
-    fseek(mvcFile, -(nbytes), SEEK_CUR);
-    fseek(mvcFile, posOFMD, SEEK_CUR);
-
-    // Load a chunk of the file starting at the position of the OFMD.
-    buffer = (char *)realloc(buffer, blockSize * sizeof(char));
-    fread(buffer, blockSize, 1, mvcFile);
-
-    // Process Data. (Right now it's just printing the total framecount.
-    // When I'm satisfied I'll try to put this data into the OFMDs array.)
-    char frameRate = buffer[4] & 15;
-
-    if (frameRate == 1) {
-      counter += 1;
-      frameCount += buffer[11] & 127;
-      // printf("Counter %d\n", counter);
-      printf("FrameCount: %d\n", frameCount);
-    }
-
-    // Seek file to the OFMD's location plus 1 byte.
-    fseek(mvcFile, -(blockSize), SEEK_CUR);
-    fseek(mvcFile, 1, SEEK_CUR);
-    free(buffer);
-
-    if (keepgoing == 0) {
       break;
     }
+    fseek(filePtr, 1, SEEK_CUR);
   }
 }
 
+FILE *searchStringInFile(char *small, int smallSize, int blockSize, FILE *file,
+                         size_t fileSize) {
+  char *buffer;
+  char *offsetPtr;
+  int smallPos;
+  int origBlockSize = blockSize;
+  size_t fileLeft;
+  bool found;
+
+  fileLeft = fileSize - ftell(file);
+  blockSize = min(blockSize, fileLeft);
+
+  // Initialize buffer.
+  buffer = (char *)malloc(blockSize * sizeof(char));
+  fread(buffer, 1, blockSize, file);
+
+  if (blockSize < origBlockSize) {
+    if ((offsetPtr = my_memmem(buffer, blockSize, small, smallSize)) != NULL) {
+      smallPos = offsetPtr - buffer;
+      fseek(file, -blockSize, SEEK_CUR);
+      fseek(file, smallPos, SEEK_CUR);
+      free(buffer);
+      return file;
+    } else {
+      free(buffer);
+      return NULL;
+    }
+  }
+
+  origBlockSize = blockSize;
+
+  // Check if 'small' exists in the current buffer.
+  if ((offsetPtr = my_memmem(buffer, blockSize, small, smallSize)) != NULL) {
+    // Set the position of 'small' within the buffer.
+    smallPos = offsetPtr - buffer;
+    found = true;
+  } else {
+    // If 'small' doesn't exist we need to search more of the file.
+    while ((offsetPtr = my_memmem(buffer, blockSize, small, smallSize)) ==
+           NULL) {
+      // Append more of the file to the buffer.
+      // While keeping track of how big the buffer gets.
+      blockSize += origBlockSize;
+      buffer = (char *)realloc(buffer, blockSize * sizeof(char));
+      fread(buffer + origBlockSize, 1, origBlockSize, file);
+    }
+    smallPos = offsetPtr - buffer;
+    found = true;
+  }
+
+  // Once the position is found seek to that location in the file.
+  fseek(file, -blockSize, SEEK_CUR);
+  fseek(file, smallPos, SEEK_CUR);
+
+  // If the string in found, return the file pointer.
+  if (found) {
+    free(buffer);
+    return file;
+  } else {
+    free(buffer);
+    return NULL;
+  }
+}
+
+// From: https://stackoverflow.com/a/46156986/13793328
+static inline size_t min(size_t x, size_t y) { return (x < y) ? x : y; }
+
+// From: https://stackoverflow.com/a/30683927/13793328
 void *my_memmem(const void *big, size_t big_len, const void *little,
                 size_t little_len) {
   void *iterator;
