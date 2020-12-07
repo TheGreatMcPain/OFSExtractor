bb42ad53e10617cbd0e75693c0a3254f4701ad2f
TheGreatMcPain <sixsupersonic@gmail.com>
2020-12-07 00:13:41 -0600

    Slightly better than original, but not as fast for some reason

--- main.c
+++ main.c
@@ -4,18 +4,14 @@
 #include <time.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024 * 1024
+// 64 Kilobytes IEC seems to be the best from my minor testing.
+#define BUFFER_SIZE 1024 * 64
 
 int getOFMDsInFile(size_t storeSize, size_t bufferSize, const char *filename,
                    unsigned char ***OFMDs);
 
 void *my_memmem(const void *haystack, size_t haystacklen, const void *needle,
                 size_t needlelen);
-
-// From: https://stackoverflow.com/a/46156986/13793328
-// Returns the smallest of two numbers.
-// (Using off_t since we're working with large files.)
-static inline off_t min(off_t x, off_t y) { return (x < y) ? x : y; }
 
 int main(int argc, const char **argv) {
   int OFMDCounter = 0;
@@ -41,8 +37,15 @@ int main(int argc, const char **argv) {
   printf("VerifiedOFMD: %d\n", verified);
 }
 
-// Search for all instances of 'pattern' in a file, and store chucks of the
-// data of size 'storeSize' which contain the pattern and data after it.
+/*
+ * Searches for all "valid" OFMDs within a 3D H264/MVC stream.
+ * Which will be later used to create OFS '3D-Planes' files.
+ *
+ * 'storeSize': Size of resulting OFMD buffers.
+ * 'bufferSize': Size of buffer that will be used when reading file in memory.
+ * 'filename': The path to the 3D H264/MVC file.
+ * 'OFMDs': Pointer to a 2D array which will contain the resulting OFMD buffers.
+ */
 int getOFMDsInFile(size_t storeSize, size_t bufferSize, const char *filename,
                    unsigned char ***OFMDs) {
 
@@ -61,7 +64,7 @@ int getOFMDsInFile(size_t storeSize, size_t bufferSize, const char *filename,
   unsigned char buffer[bufferSize];
 
   int progress = 0;
-  int timeoutCounter = 0;
+  int timeoutCounter = time(NULL);
   const int timeout = 60;
 
   FILE *filePtr = fopen(filename, "rb");
@@ -80,31 +83,26 @@ int getOFMDsInFile(size_t storeSize, size_t bufferSize, const char *filename,
     // bufferSize.
     memmove(buffer, buffer + (bufferSize / 2), (bufferSize / 2) * sizeByte);
     fread(buffer + (bufferSize / 2), sizeByte, (bufferSize / 2), filePtr);
+    if ((timeoutCounter - time(NULL)) > timeout) {
+      fprintf(stderr, "SEI couldn't be found within %d seconds.\n", timeout);
+      return 1;
+    }
   }
 
-  while (!feof(filePtr)) {
-    match = my_memmem(buffer, bufferSize, seiString, seiSize);
+  while (match != NULL) {
+    // Make match the start of buffer, then fill the rest of the buffer.
+    matchPos = (match - buffer);
+    memmove(buffer, match, (bufferSize - matchPos));
+    fread(buffer + (bufferSize - matchPos), sizeByte, matchPos, filePtr);
 
-    // Check if we found seiString.
+    // Search for OFMD within the next 200 bytes from the seiString.
+    match = my_memmem(buffer, 200, "OFMD", 4);
     if (match != NULL) {
-      // Make match the start of buffer, then fill the rest of the buffer.
+      // Move OFMD to start of buffer.
       matchPos = (match - buffer);
       memmove(buffer, match, (bufferSize - matchPos));
       fread(buffer + (bufferSize - matchPos), sizeByte, matchPos, filePtr);
 
-      // Search for OFMD within the next 200 bytes from the seiString.
-      match = my_memmem(buffer, 200, "OFMD", 4);
-      if (match != NULL) {
-        // Move OFMD to start of buffer.
-        matchPos = (match - buffer);
-        memmove(buffer, match, (bufferSize - matchPos));
-        fread(buffer + (bufferSize - matchPos), sizeByte, matchPos, filePtr);
-      } else {
-        // Skip if the OFMD is not valid.
-        memmove(buffer, buffer + 200, bufferSize - 200);
-        fread(buffer + (bufferSize - 200), sizeByte, 200, filePtr);
-        continue;
-      }
       // Make sure the OFMD is valid before loading it into the OFMDs.
       frameRate = buffer[4] & 15;
       if (frameRate >= 1 && frameRate <= 7 && frameRate != 5) {
@@ -116,49 +114,30 @@ int getOFMDsInFile(size_t storeSize, size_t bufferSize, const char *filename,
         memcpy((*OFMDs)[numOfBuffers++], buffer, storeSize);
       }
     } else {
+      // Skip if the OFMD is not valid.
+      memmove(buffer, buffer + 200, bufferSize - 200);
+      fread(buffer + (bufferSize - 200), sizeByte, 200, filePtr);
+    }
+
+    timeoutCounter = time(NULL);
+    while ((match = my_memmem(buffer, bufferSize, seiString, seiSize)) ==
+           NULL) {
+      if (feof(filePtr)) {
+        break;
+      }
       // If no seiString was found. Just shift the buffer by half the
       // bufferSize.
       memmove(buffer, buffer + (bufferSize / 2), (bufferSize / 2) * sizeByte);
       fread(buffer + (bufferSize / 2), sizeByte, (bufferSize / 2), filePtr);
+      if ((timeoutCounter - time(NULL)) > timeout) {
+        fprintf(stderr, "SEI couldn't be found within %d seconds.\n", timeout);
+        return 1;
+      }
     }
 
     progress = (float)ftello(filePtr) / (float)fileSize * 100;
     fprintf(stderr, "\rProgress: %d%s", progress, "%");
   }
-
-  // Make sure all OFMDs are accounted for.
-  while ((match = my_memmem(buffer, bufferSize, seiString, seiSize)) != NULL) {
-    matchPos = (match - buffer);
-    bufferSize -= matchPos;
-    memmove(buffer, match, bufferSize);
-
-    match = my_memmem(buffer, 200, "OFMD", 4);
-    if (match != NULL) {
-      matchPos = (match - buffer);
-      bufferSize -= matchPos;
-      memmove(buffer, match, bufferSize);
-
-      frameRate = buffer[4] & 15;
-      if (frameRate >= 1 && frameRate <= 7 && frameRate != 5) {
-        *OFMDs =
-            (unsigned char **)realloc(*OFMDs, sizeBypePtr * (numOfBuffers + 1));
-        (*OFMDs)[numOfBuffers] = (unsigned char *)malloc(sizeByte * storeSize);
-        memcpy((*OFMDs)[numOfBuffers++], buffer, min(bufferSize, storeSize));
-      } else {
-        // Skip if the OFMD is not valid.
-        bufferSize -= 200;
-        memmove(buffer, buffer + 200, bufferSize);
-        fread(buffer + (bufferSize), sizeByte, 200, filePtr);
-        continue;
-      }
-    } else {
-      // Skip if the OFMD is not valid.
-      bufferSize -= seiSize;
-      memmove(buffer, buffer + seiSize, bufferSize);
-      fread(buffer + (bufferSize), sizeByte, seiSize, filePtr);
-    }
-  }
-
   progress = (float)ftello(filePtr) / (float)fileSize * 100;
   fprintf(stderr, "\rProgress: %d%s", progress, "%");
   fprintf(stderr, "\n");
