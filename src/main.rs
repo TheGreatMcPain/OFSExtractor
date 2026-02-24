fd3c78fd2f364ab90b01b9c4038face0f60c2ee3
TheGreatMcPain <james@thegreatmcpain.xyz>
2026-02-23 23:17:54 -0600

    Fix clippy issues, add comment showing OFS file example

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn verify_planes(ofmd_data: &mut OFMDdata) {
         if there_are_planes {
             println!();
             println!("3D-Plane #{}", x);
-            parse_depths(&ofmd_data, x);
+            parse_depths(ofmd_data, x);
         } else {
             println!();
             println!("3D-Plane #{} is empty.", x);
@@ -62,11 +62,9 @@ fn compare_depths(ofmd_data: &OFMDdata, plane_num: usize) {
     let mut same_plane = false;
 
     for x in 0..ofmd_data.num_of_planes {
-        if ofmd_data.planes[plane_num] == ofmd_data.planes[x] {
-            if x != plane_num {
-                message_string += &format!(" {}", x).to_string();
-                same_plane = true;
-            }
+        if ofmd_data.planes[plane_num] == ofmd_data.planes[x] && x != plane_num {
+            message_string += &format!(" {}", x).to_string();
+            same_plane = true;
         }
     }
 
@@ -114,7 +112,7 @@ fn parse_depths(ofmd_data: &OFMDdata, plane_num: usize) {
         if byte > maxval {
             maxval = byte;
         }
-        total += byte as i32;
+        total += byte;
     }
 
     let average: f32 = total as f32 / (ofmd_data.total_frames as f32 - undefined as f32);
@@ -125,7 +123,7 @@ fn parse_depths(ofmd_data: &OFMDdata, plane_num: usize) {
     println!("Number of changes of depth value: {}", cuts);
     println!("First frame with defined depth: {}", firstframe);
     println!("Last frame with defined depth: {}", lastframe);
-    compare_depths(&ofmd_data, plane_num);
+    compare_depths(ofmd_data, plane_num);
     if minval == maxval {
         println!(
             "*** Warning This 3D-Plane has a fixed depth of {}! ***",
@@ -154,18 +152,18 @@ fn parse_ofmds(ofmd_array: &mut Vec<Vec<u8>>) -> Box<OFMDdata> {
     let mut total_frames: usize = 0;
     for ofmd in ofmd_array {
         let frame_count: usize = ofmd[11] as usize & 127;
-        for plane in 0 as usize..ofmd_data.num_of_planes as usize {
+        for plane in 0..ofmd_data.num_of_planes {
             counter = total_frames;
             let start = 14 + (plane * frame_count);
             let end = 14 + (plane * frame_count) + frame_count;
-            for y in start..end {
-                ofmd_data.planes[plane][counter] = ofmd[y] as u8;
+            for y in ofmd.iter().take(end).skip(start) {
+                ofmd_data.planes[plane][counter] = *y;
                 counter += 1;
             }
         }
         total_frames += frame_count
     }
-    return ofmd_data;
+    ofmd_data
 }
 
 fn get_ofmd_from_sei<'a>(sei: &'a SeiMessage, buf: &'a mut Vec<u8>) -> bool {
@@ -182,13 +180,13 @@ fn get_ofmd_from_sei<'a>(sei: &'a SeiMessage, buf: &'a mut Vec<u8>) -> bool {
 
     // Do a crude check by validating the frame rate value.
     let frame_rate = payload_string[4] & 15;
-    if !(frame_rate >= 1 && frame_rate <= 7 && frame_rate != 5) {
+    if !(1..=7).contains(&frame_rate) && frame_rate != 5 {
         return false;
     }
 
     *buf = payload_string.to_vec();
 
-    return true;
+    true
 }
 
 fn get_ofmds_in_file(path: String, ofmd_array: &mut Vec<Vec<u8>>) {
@@ -200,32 +198,27 @@ fn get_ofmds_in_file(path: String, ofmd_array: &mut Vec<Vec<u8>>) {
         let nal_header = nal.header().unwrap();
         let nal_unit_type = nal_header.nal_unit_type();
 
-        match nal_unit_type {
-            UnitType::SEI => {
-                let mut scratch = vec![];
-                let mut reader = SeiReader::from_rbsp_bytes(nal.rbsp_bytes(), &mut scratch);
-                loop {
-                    match reader.next() {
-                        Ok(Some(sei)) => match sei.payload_type {
-                            HeaderType::MvcScalableNesting => {
-                                let mut buf: Vec<u8> = vec![];
+        if nal_unit_type == UnitType::SEI {
+            let mut scratch = vec![];
+            let mut reader = SeiReader::from_rbsp_bytes(nal.rbsp_bytes(), &mut scratch);
+            loop {
+                match reader.next() {
+                    Ok(Some(sei)) => {
+                        if sei.payload_type == HeaderType::MvcScalableNesting {
+                            let mut buf: Vec<u8> = vec![];
 
-                                if get_ofmd_from_sei(&sei, &mut buf) {
-                                    ofmd_array.push(buf)
-                                }
+                            if get_ofmd_from_sei(&sei, &mut buf) {
+                                ofmd_array.push(buf)
                             }
-                            _ => {}
-                        },
-                        Ok(None) => break,
-                        Err(e) => {
-                            println!("{:?}", e);
                         }
+                    }
+                    Ok(None) => break,
+                    Err(e) => {
+                        println!("{:?}", e);
                     }
                 }
             }
-            _ => {}
         }
-
         NalInterest::Ignore
     });
 
@@ -253,7 +246,7 @@ fn get_ofmds_in_file(path: String, ofmd_array: &mut Vec<Vec<u8>>) {
             break;
         }
 
-        reader.push(&buf);
+        reader.push(buf);
         if !use_stdin {
             file_position += buf_len;
             progress = ((file_position as f32 / file_size as f32) * 100.0) as i32;
@@ -271,10 +264,33 @@ fn get_ofmds_in_file(path: String, ofmd_array: &mut Vec<Vec<u8>>) {
 
 fn create_ofs_files(ofmd_data: &OFMDdata, out_directory: String, drop_frame: bool) {
     // Structure of an OFS file.
+    //
+    // Pulled from BD3D2MK3D's 3DPlanes2OFS sources. (thanks, r0lZ!)
+    //
+    // filename.ofs {
+    // 	signature               8*8  (8) : 0x894f46530d0a1a0a
+    // 	version                 8*4  (4) : "0100" coded according to ISO 646 : 0x30313030
+    // 	guid                   8*16 (16) : random number based on the 3D-Plane number?
+    // 	frame_rate                4  (1) : 1 ?  -> 0x10
+    // 	drop_frame_flag           4   '  : 0
+    // 	number_of_rolls           8  (1) : 1 ?  -> 0x01
+    // 	reserved                8*2  (2) : 0x00 -> 0x0000
+    // 	for(i=0; i<number_of_rolls; i++){
+    // 		marker_bits         1*8  (1) : '00000000' -> 0x00
+    // 		start_timecode      4*8  (4) : 0x00000000 ?
+    // 		number_of_frames     32  (4) : [string length $plane]
+    // 		# Total bytes in the header: 41
+    // 		# Here comes the 3D-Plane itself, one byte per frame.
+    // 		for(j=0; j<number_of_frames; j++){
+    // 			offset_direction  1  (1) : "sign" of the 3D depth offset
+    // 			offset_value      7   '  : The 3D offset value
+    // 		}
+    // 	}
+    // }
+    //
     let signature: [u8; 8] = [0x89, 0x4f, 0x46, 0x53, 0x0d, 0x0a, 0x1a, 0x0a];
     let version: [u8; 4] = [0x30, 0x31, 0x30, 0x30];
-    let mut guid: [u8; 16] = [0; 16];
-    let frame_rate: u8;
+    let mut guid: [u8; 16] = std::array::from_fn(|_| rand::rng().random::<u8>());
     let rolls_and_reserved: [u8; 4] = [0x01, 0x00, 0x00, 0x00];
     let timecode: [u8; 4] = [0x00, 0x00, 0x00, 0x00];
     let mut frame_array: [u8; 4] = [0x00, 0x00, 0x00, 0x00];
@@ -288,18 +304,13 @@ fn create_ofs_files(ofmd_data: &OFMDdata, out_directory: String, drop_frame: boo
     buffer.extend_from_slice(&signature);
     buffer.extend_from_slice(&version);
 
-    // Generate GUID. The last value will be the plane number.
-    for x in 0..15 {
-        guid[x] = rand::rng().random::<u8>();
-    }
-
     // Store the framecount in 4 bytes.
     frame_array[3] = (ofmd_data.total_frames % 256) as u8;
     frame_array[2] = ((ofmd_data.total_frames >> 8) % 256) as u8;
     frame_array[1] = ((ofmd_data.total_frames >> 16) % 256) as u8;
     frame_array[0] = ((ofmd_data.total_frames >> 24) % 256) as u8;
 
-    frame_rate = (ofmd_data.frame_rate * 16) + drop_frame as u8;
+    let frame_rate = (ofmd_data.frame_rate * 16) + drop_frame as u8;
 
     for plane in 0..ofmd_data.num_of_planes {
         if ofmd_data.valid_planes[plane] {
@@ -313,7 +324,7 @@ fn create_ofs_files(ofmd_data: &OFMDdata, out_directory: String, drop_frame: boo
             buffer.extend_from_slice(&frame_array);
 
             for x in 0..ofmd_data.total_frames {
-                buffer.push(ofmd_data.planes[plane][x] as u8);
+                buffer.push(ofmd_data.planes[plane][x]);
             }
 
             let out_path = path.join(format!("3D-Planes-{:02}.ofs", plane));
