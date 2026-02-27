7425ff16ceb9621b974d7458492ac6ee8f82cabb
TheGreatMcPain <james@thegreatmcpain.xyz>
2026-02-27 06:08:46 -0600

    Make clippy happy

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let path = std::path::Path::new(&out_dir).join("license.rs");
 
-    let new_func = "pub fn get_license() -> &'static str { return \n\"".to_string();
+    let new_func = "pub fn get_license() -> &'static str { \n\"".to_string();
     let close_func = "\"\n}".to_string();
     let mut license_string = std::fs::read_to_string("LICENSE").expect("Error reading 'LICENSE'");
     license_string = license_string.replace("\"", "\\\"");
